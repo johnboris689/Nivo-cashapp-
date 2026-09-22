ef97cdfedc7cbaf4c54bc7c939b0f327830a1b2e
johnboris689 <talkdavidjohn@gmail.com>
2026-09-22 12:05:53 +0100

    Fix admin delete user build error

--- server.ts
+++ server.ts
@@ -5783,7 +5783,7 @@ app.post('/api/admin/users/reset-pin', authenticateAdminToken, (req, res) => {
 });
 
 // Delete account by Admin
-app.post('/api/admin/users/delete', authenticateAdminToken, (req, res) => {
+app.post('/api/admin/users/delete', authenticateAdminToken, async (req, res) => {
   const { email } = req.body;
   if (!email) {
     return res.status(400).json({ error: 'Please supply email.' });
